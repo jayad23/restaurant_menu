75f63c177a7d7e684ad45f31fe5b95068402ffc7
jayad23 <jaidervanegas@live.com>
2022-08-10 03:35:18 +0200

    Se implementa Formik y Yup para validar inputs de Login.

--- src/views/Common/CustomButton/CustomButton.js
+++ src/views/Common/CustomButton/CustomButton.js
@@ -3,10 +3,10 @@ import { Button } from 'reactstrap'
 import styles from "./Custombutton.module.scss"
 
 const CustomButton = (props) => {
-    const { text, event } = props
+    const { text, event, type } = props
     return (
         <Fragment>
-            <Button.Ripple onClick={event} className={styles['login-custom-button']}>
+            <Button.Ripple type={type} onClick={event} className={styles['login-custom-button']}>
                 {text}
             </Button.Ripple>
         </Fragment>
--- src/views/Pages/Login/Login.js
+++ src/views/Pages/Login/Login.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { FormGroup, Label, Input, Button, CardImg } from "reactstrap"
+import { Label, Input, CardImg } from "reactstrap"
 import { Link } from "react-router-dom"
 import { FcGoogle } from "react-icons/fc"
 import styles from "./Login.module.scss"
@@ -7,19 +7,27 @@ import AuthLayout from '../../Common/AutLayout'
 import logo from "../../../assets/images/logo/logo_login.png"
 import CustomFormGroup from '../../Common/FormGroup/FormGroup'
 import CustomButton from '../../Common/CustomButton/CustomButton'
+import Error from '../../Common/ErrorMessage/ErrorMessage'
 
-const Login = () => {
+const Login = (props) => {
+
+    const { handleSubmit, values, setFieldValue, errors } = props
     
     return (
         <AuthLayout>
             <div className='text-center'>
                 <CardImg className="main-logo" top src={logo} alt="card1" style={{ width: "100px"}}/>
             </div>
-            <form onSubmit={(e) => e.preventDefault()}>
+            <form onSubmit={handleSubmit}>
                 <CustomFormGroup>
                     <Label>Correo Electrónico:</Label>
-                    <Input 
+                    <Input
+                        value={values?.email || ""}
                         placeholder='example@example.com'
+                        onChange={(e) => setFieldValue("email", e.target.value)}
+                    />
+                    <Error 
+                        errorMessage={errors?.email}
                     />
                 </CustomFormGroup>
                 <CustomFormGroup>
@@ -30,13 +38,19 @@ const Login = () => {
                         </Link>
                     </div>
                     <Input 
-                        placeholder='example@example.com'
+                        value={values?.password}
+                        placeholder='**********'
+                        onChange={(e) => setFieldValue("password", e.target.value)}
+                    />
+                    <Error 
+                        errorMessage={errors?.password}
                     />
                 </CustomFormGroup>
                 <CustomFormGroup extraClassname='text-center'>
                     <CustomButton 
+                        type="submit"
                         text="ingresar"
-                        event={() => console.log("Logged")}
+                        event={undefined}
                     />
                 </CustomFormGroup>
                 <CustomFormGroup extraClassname='text-center'>
